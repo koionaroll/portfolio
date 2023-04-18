6024606e134bbd0889971337f5f61668286cb255
Khoi Tran <tranvankhoi2002@gmail.com>
2023-04-18 11:52:16 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.scss";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contacts from "./components/Contact";
+
+function App(){
+  return(
+  <>
+    <About />
+    <Projects />
+    <Contacts />
+  </>
+  )
+}
+
+export default App;
